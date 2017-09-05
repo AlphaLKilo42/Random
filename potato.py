c894087917c4bc537d92f20efae57ce21640116c
Al Kuban <31545326+AlphaLKilo42@users.noreply.github.com>
2017-09-05 16:12:26 +0000

    Add files via upload

---
+++ potato.py
@@ -0,0 +1,8 @@
+print ("i r8 8/8, don't h8, aprreci8, and don't forget to r8, m8")
+count = 1
+import time
+while True:
+	print(count);print ("potato")
+	count = count + 1
+	if count > 12: break
+print("RIP Potato :'(")
