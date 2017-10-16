8ddf369585dc7872d0326132ad3a3f01bfe43d8d
Al Kuban <31545326+AlphaLKilo42@users.noreply.github.com>
2017-10-16 10:16:04 -0700

    Update potato.py

--- potato.py
+++ potato.py
@@ -2,7 +2,8 @@ print ("i r8 8/8, don't h8, aprreci8, and don't forget to r8, m8")
 count = 1
 import time
 while True:
-	print(count);print ("potato")
+	print (count, "potato");
+	time.sleep(0.75)
 	count = count + 1
 	if count > 12: break
 print("RIP Potato :'(")
